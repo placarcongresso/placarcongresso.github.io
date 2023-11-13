ac7204d43bb2cff4bdeb1b3d692bd884d49b1c3b
Placar Congresso <admin@placarcongresso.com>
2023-11-12 23:49:39 -0300

    chore: add footnote, improve responsiveness

--- js/layout.js
+++ js/layout.js
@@ -1,10 +1,21 @@
-const iframeElement = document.getElementById('content-iframe');
+const iframeElement = document.getElementById('iframe-ranking');
 
 window.addEventListener('resize', () => {
   const iframeWidth = iframeElement.offsetWidth;
-  const iframeHeight = iframeWidth * 6.90; // Adjust this value for desired aspect ratio
+  const iframeHeight = iframeWidth * 6.95;
 
   iframeElement.style.height = iframeHeight + 'px';
+});
+
+window.dispatchEvent(new Event('resize')); // Trigger initial resize event
+
+const content = document.getElementById('content-iframe');
+
+window.addEventListener('resize', () => {
+  const iframeWidth = content.offsetWidth;
+  const iframeHeight = iframeWidth * 0.68;
+
+  content.style.height = iframeHeight + 'px';
 });
 
 window.dispatchEvent(new Event('resize')); // Trigger initial resize event
