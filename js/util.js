289320caa990eef739a9e37eb78b72849ec3057e
Placar Congresso <admin@placarcongresso.com>
2024-01-13 14:00:03 -0300

    refactor: remove code duplication in tests

--- js/util.js
+++ js/util.js
@@ -2,6 +2,11 @@ function isMobile(window) {
   return window.matchMedia('screen and (max-width: 768px)').matches;
 }
 
+function rankingURL(window) {
+  return "https://app.powerbi.com/view?r=eyJrIjoiOTYwYmYyYWEtYzRmYi00M2EwLWFjYjgtYjJiMDc5ZTg0ODk4IiwidCI6IjhhN2VkZTZhLWU0MjgtNGE5ZS04NzJkLWIwMjdkNGUxMDM1MCJ9&pageName=ReportSection71b50ba615d17479414a";
+}
+
 module.exports = {
   isMobile,
+  rankingURL,
 };
--- js/util.test.js
+++ js/util.test.js
@@ -2,56 +2,61 @@
  * @jest-environment jsdom
  */
 
-const { isMobile } = require('./util');
+const { isMobile, rankingURL } = require('./util');
 
 describe('isMobile', () => {
-  wantMaxWidth = 768;
-
-  test('should return true if width is less than or equal to 768px', () => {
-    Object.defineProperty(window, 'innerWidth', {
-      writable: true,
-      configurable: true,
-      value: 720,
-    });
-
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation(query => ({
-        matches: window.innerWidth <= wantMaxWidth,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
+  test('should return true if width is less than 768px', () => {
+    setWindowWidth(window, 720);
 
     expect(isMobile(window)).toBe(true);
   });
 
-    test('should return false if width is greater than 768px', () => {
-    Object.defineProperty(window, 'innerWidth', {
-      writable: true,
-      configurable: true,
-      value: 800,
-    });
+  test('should return true if width is equal to 768px', () => {
+    setWindowWidth(window, 768);
 
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation(query => ({
-        matches: window.innerWidth <= wantMaxWidth,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      })),
-    });
+    expect(isMobile(window)).toBe(true);
+  });
+
+  test('should return false if width is greater than 768px', () => {
+    setWindowWidth(window, 800);
 
     expect(isMobile(window)).toBe(false);
   });
 });
+
+
+describe('rankingURL', () => {
+  wantMaxWidth = 768;
+
+  mobileURL = "https://app.powerbi.com/view?r=eyJrIjoiOTYwYmYyYWEtYzRmYi00M2EwLWFjYjgtYjJiMDc5ZTg0ODk4IiwidCI6IjhhN2VkZTZhLWU0MjgtNGE5ZS04NzJkLWIwMjdkNGUxMDM1MCJ9&pageName=ReportSection71b50ba615d17479414a"
+
+  test('should return mobile version when on mobile', () => {
+    setWindowWidth(window, 720);
+
+    expect(rankingURL(window)).toBe(mobileURL);
+  });
+});
+
+
+function setWindowWidth(window, width) {
+  mobileWidth = 768;
+
+  Object.defineProperty(window, 'innerWidth', {
+    writable: true,
+    configurable: true,
+    value: width,
+  });
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+      matches: window.innerWidth <= mobileWidth,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+}
