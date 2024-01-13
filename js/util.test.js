56fda0153c654a5a690dc48ed13aa2ab257143c9
Placar Congresso <admin@placarcongresso.com>
2024-01-13 15:00:50 -0300

    chore: use ES6 modules in jest

--- js/util.test.js
+++ js/util.test.js
@@ -2,50 +2,50 @@
  * @jest-environment jsdom
  */
 
-const { isMobile, rankingURL } = require('./util');
+import * as util from './util';
 
 describe('isMobile', () => {
   test('should return true if width is less than 768px', () => {
     setWindowWidth(window, 720);
 
-    expect(isMobile(window)).toBe(true);
+    expect(util.isMobile(window)).toBe(true);
   });
 
   test('should return true if width is equal to 768px', () => {
     setWindowWidth(window, 768);
 
-    expect(isMobile(window)).toBe(true);
+    expect(util.isMobile(window)).toBe(true);
   });
 
   test('should return false if width is greater than 768px', () => {
     setWindowWidth(window, 800);
 
-    expect(isMobile(window)).toBe(false);
+    expect(util.isMobile(window)).toBe(false);
   });
 });
 
 
 describe('rankingURL', () => {
-  mobileURL = "https://app.powerbi.com/view?r=eyJrIjoiOTYwYmYyYWEtYzRmYi00M2EwLWFjYjgtYjJiMDc5ZTg0ODk4IiwidCI6IjhhN2VkZTZhLWU0MjgtNGE5ZS04NzJkLWIwMjdkNGUxMDM1MCJ9&pageName=ReportSection71b50ba615d17479414a"
+  let mobileURL = "https://app.powerbi.com/view?r=eyJrIjoiOTYwYmYyYWEtYzRmYi00M2EwLWFjYjgtYjJiMDc5ZTg0ODk4IiwidCI6IjhhN2VkZTZhLWU0MjgtNGE5ZS04NzJkLWIwMjdkNGUxMDM1MCJ9&pageName=ReportSection71b50ba615d17479414a"
 
-  desktopURL = "https://app.powerbi.com/view?r=eyJrIjoiOTYwYmYyYWEtYzRmYi00M2EwLWFjYjgtYjJiMDc5ZTg0ODk4IiwidCI6IjhhN2VkZTZhLWU0MjgtNGE5ZS04NzJkLWIwMjdkNGUxMDM1MCJ9&pageName=ReportSectioncab5881aaee03387ae73"
+  let desktopURL = "https://app.powerbi.com/view?r=eyJrIjoiOTYwYmYyYWEtYzRmYi00M2EwLWFjYjgtYjJiMDc5ZTg0ODk4IiwidCI6IjhhN2VkZTZhLWU0MjgtNGE5ZS04NzJkLWIwMjdkNGUxMDM1MCJ9&pageName=ReportSectioncab5881aaee03387ae73"
 
   test('should return mobile version when on mobile', () => {
     setWindowWidth(window, 720);
 
-    expect(rankingURL(window)).toBe(mobileURL);
+    expect(util.rankingURL(window)).toBe(mobileURL);
   });
 
   test('should return desktop version when not on mobile', () => {
     setWindowWidth(window, 800);
 
-    expect(rankingURL(window)).toBe(desktopURL);
+    expect(util.rankingURL(window)).toBe(desktopURL);
   });
 });
 
 
 function setWindowWidth(window, width) {
-  mobileWidth = 768;
+  let mobileWidth = 768;
 
   Object.defineProperty(window, 'innerWidth', {
     writable: true,
