b19d00aee03e9575ba15974b07de571f5d602978
Placar Congresso <admin@placarcongresso.com>
2024-06-10 09:16:19 -0300

    add custom aspect ratio for senate ranking

--- js/layout.js
+++ js/layout.js
@@ -14,6 +14,22 @@ if (iframeElement) {
   });
 }
 
+const iframeElementSenate = document.getElementById('iframe-ranking-senate');
+if (iframeElementSenate) {
+  window.addEventListener('resize', () => {
+    const iframeWidth = iframeElementSenate.offsetWidth;
+    const iframeHeight = iframeWidth * 1.6;
+
+    iframeElementSenate.style.height = iframeHeight + 'px';
+  });
+  window.dispatchEvent(new Event('resize')); // Trigger initial resize event
+
+  const loadingCircle = document.getElementById('loading-div');
+  iframeElementSenate.addEventListener('load', () => {
+    loadingCircle.style.display = 'none';
+  });
+}
+
 const contentIframe = document.getElementById('content-iframe');
 if (contentIframe) {
   window.addEventListener('resize', () => {
